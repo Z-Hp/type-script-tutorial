c2efed959cd396bf4ab947042b0e9fbab4992b8f
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-02-18 11:53:06 -0800

    added Invoice class

--- public/index.js
+++ public/index.js
@@ -1,4 +1,16 @@
 "use strict";
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.detail = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " recieve ").concat(this.amount, " for ").concat(this.detail);
+    };
+    return Invoice;
+}());
+var invoices = [];
 var form = document.querySelector("form");
 var type = document.querySelector("#type");
 var tofrom = document.querySelector("#tofrom");
@@ -6,5 +18,11 @@ var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("".concat(type.value, ": \u0645\u0628\u0644\u063A ").concat(amount.value, " \u0628\u0647 ").concat(tofrom.value, " \u0628\u0631\u0627\u06CC ").concat(details.value, " \u067E\u0631\u062F\u0627\u062E\u062A \u0634\u062F"));
+    // console.log(`${type.value}: مبلغ ${amount.value} به ${tofrom.value} برای ${details.value} پرداخت شد`)
+    var invTwo = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    invoices.push(invTwo);
+    console.log(invoices);
 });
+// const invOne = new Invoice("roya", "teaching", 800);
+// console.log(invOne.format());
+// invoices.push(invOne);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
-const form = document.querySelector("form")!;
 
-const type = document.querySelector("#type") as HTMLSelectElement;
-const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
-const details = document.querySelector("#details") as HTMLInputElement;
-const amount = document.querySelector("#amount") as HTMLInputElement;
+
+class Invoice {
+    client: string;
+    detail: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.detail = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} recieve ${this.amount} for ${this.detail}`
+    }
+}
+
+let invoices : Invoice[] = [];
+
+var form = document.querySelector("form")!;
+var type = document.querySelector("#type") as HTMLSelectElement;
+var tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+var details = document.querySelector("#details") as HTMLInputElement;
+var amount = document.querySelector("#amount") as HTMLInputElement;
 
 form.addEventListener("submit", (event: Event) => {
     event.preventDefault();
-    console.log(`${type.value}: مبلغ ${amount.value} به ${tofrom.value} برای ${details.value} پرداخت شد`)
+    // console.log(`${type.value}: مبلغ ${amount.value} به ${tofrom.value} برای ${details.value} پرداخت شد`)
+    const invTwo = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    invoices.push(invTwo);
+    console.log(invoices);
+    
+});
 
-})
+// const invOne = new Invoice("roya", "teaching", 800);
+
+// console.log(invOne.format());
+
+
+// invoices.push(invOne);
+
